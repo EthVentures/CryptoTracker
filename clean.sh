a026ceec2b94b8180d5c2e80281e2505d33d44dc
Dave Conroy <dconroy@gmail.com>
2017-07-29 13:09:51 -0500

    typo in clean boot script

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker stop cryptotrack_app_1 cryptotrack_kibana_1 cryptotrack_elasticsearch_1
-docker rm cryptotrack_app_1 cryptotrack_kibana_1 cryptotrack_elasticsearch_1
+docker stop cryptotracker_app_1 cryptotracker_kibana_1 cryptotracker_elasticsearch_1
+docker rm cryptotracker_app_1 cryptotracker_kibana_1 cryptotracker_elasticsearch_1
 docker-compose build && docker-compose up
