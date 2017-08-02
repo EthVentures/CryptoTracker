1597660f477a2c1bdbe006956451118abdfd5436
Dave Conroy <dconroy@gmail.com>
2017-08-01 20:44:20 -0500

    kibana indexes are now created dynamically

--- app/settings.py
+++ app/settings.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import Dotenv
-dotenv = Dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+dotenv = Dotenv(os.path.join(os.path.dirname(__file__), "default.env"))
 os.environ.update(dotenv)
 
-MARKET_REFRESH_RATE=15
-RETRY_RATE=20
+#timers in seconds
+MARKET_REFRESH_RATE=1
+RETRY_RATE=5
+API_TIMEOUT=2
 
 BITFINEX_API_URL = os.environ.get("BITFINEX_API_URL")
 BITTREX_API_URL = os.environ.get("BITTREX_API_URL")
@@ -17,4 +19,3 @@ LOGLEVEL = os.environ.get("TRACKER_LOG_LEVEL")
 INITIAL_SLEEP = int(os.environ.get("INITIAL_SLEEP"))
 
 ELASTICSEARCH_CONNECT_STRING = os.environ.get("ELASTICSEARCH_CONNECT_STRING")
-INITIAL_INDEX_ARRAY = ['eth.gdax.ticker','eth.gemini.ticker','eth.bitfinex.ticker','eth.bittrex.ticker','eth.poloniex.ticker']
--- app/utils.py
+++ app/utils.py
@@ -6,22 +6,15 @@ Created on Fri Jun  9 19:29:13 2017
 @authors: avelkoski
          dconroy
 """
-from elasticsearch import Elasticsearch, helpers
-import os
-import sys
-import traceback
-import logging
+import elasticsearch
 import time
+import logging
 
 def generate_nonce():
     return int(time.time()*10)
 
-def create_indices(es,index_array):
-        for index in index_array:
-            try:
-                if (es.indices.exists(index)):
-                    logging.info('Already Exists, Skipping:! ' + index)
-                else:
-                    es.indices.create(index)
-            except Exception as e:
-                logging.info(e)
+def create_index(es,index):
+        if (es.indices.exists(index)):
+            logging.info('Already Exists, Skipping:! ' + index)
+        else:
+            es.indices.create(index)
